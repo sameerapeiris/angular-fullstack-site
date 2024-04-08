fda60b7a891211ea5d357021f5ed72382ba34e15
sameerapeiris <sameedsdp@gmail.com>
2024-04-08 09:22:55 +0300

    fake data added

---
+++ src/app/fake-data.ts
@@ -0,0 +1,16 @@
+import { Listing } from './types';
+
+export const fakeListing: Listing[] = [
+  {
+    id: '123',
+    name: 'Jhone Doe',
+    description: 'CEO of the company',
+    price: 2000,
+  },
+  {
+    id: '245',
+    name: 'Shane Paul',
+    description: 'Director of the company',
+    price: 300,
+  },
+];
